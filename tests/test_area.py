2a162fc79e8690224d636b599059da2f5242fd8d
Reeba Mary Betty <reebadarson17@gmail.com>
2024-03-22 21:21:34 -0400

    Update test_area.py

--- tests/test_area.py
+++ tests/test_area.py
@@ -1,6 +1,6 @@
 import pytest  
 from src.area import calculate_area_square  
-  
+#function
 def test_calculate_area_square():  
     assert calculate_area_square(2) == 4  
     assert calculate_area_square(2.5) == 6.25
